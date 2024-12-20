0b382f330b036592abf74bcc17547b542035f9a2
Tasnia Binte Ashek <144607723+tasniabinte@users.noreply.github.com>
2024-12-20 19:08:22 +0600

    Add files via upload

---
+++ t1.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('reveal-button').addEventListener('click', () => {
+    const surprise = document.getElementById('surprise');
+    surprise.classList.toggle('hidden');
+});
